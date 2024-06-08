f591d8f64314164c3f8a5c6eee1e79bf17168610
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-06-08 09:38:07 -0700

    updated the readme document

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+
 require('dotenv').config(); //imports .env
+
+console.log(process.env.MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true
